9f016ce3ee5a8bd4d899b792ceb59eea5974c2b6
takam5f2 <taka.akamine@gmail.com>
2018-12-02 23:47:56 +0900

    update particle fileter.cpp and now debugging it.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -41,6 +41,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
       
     particles.push_back(particle);
   }
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -52,9 +53,9 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
   normal_distribution<double> dist_velocity(velocity, std_pos[0]);
   normal_distribution<double> dist_yaw_rate(yaw_rate, std_pos[2]);
 
-  for (unsigned int i = 0; i < this->num_particles; i++) {
+  for (int i = 0; i < this->num_particles; i++) {
     double vel = dist_velocity(rand_eng);
-    double yrate = dist_yaw_rate(yaw_rate);
+    double yrate = dist_yaw_rate(rand_eng);
     double new_theta = particles[i].theta + yrate * delta_t;
     double new_x = particles[i].x + vel / yrate * (sin(new_theta) - sin(particles[i].theta));
     double new_y = particles[i].y + vel / yrate * (cos(particles[i].theta) - cos(new_theta));
@@ -70,10 +71,10 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	//   observed measurement to this particular landmark.
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
-  for (unsigned int i = 0; i < predicted.size(); i++) {
+  for (int i = 0; i < predicted.size(); i++) {
     LandmarkObs *roi = &predicted[i];
     double min_dist = 0;
-    for (unsigned int j = 0; j < observations.size(); j++) {
+    for (int j = 0; j < observations.size(); j++) {
       double dist = pow((roi->x - observations[i].x), 2) + pow((roi->y - observations[i].y), 2);
       if (j == 0) {
         observations[j].id = roi->id;
@@ -84,8 +85,8 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
           min_dist = dist;
         } // if (dist < min_dist)
       } // if (i == 0)
-    } // for (unsigned int j = 0; j < observations.size(); j++)
-  } // for (unsigned int i = 0; i < predicted.size(); i++)
+    } // for (int j = 0; j < observations.size(); j++)
+  } // for (int i = 0; i < predicted.size(); i++)
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -100,13 +101,87 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   and the following is a good resource for the actual equation to implement (look at equation 
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
+  std::vector<LandmarkObs> roi_landmarks;
+  std::vector<LandmarkObs> trans_observations;
+  double weights_sum = 0;
+
+  weights.clear();
+  for (int i = 0; i < this->num_particles; i++) {
+    roi_landmarks.clear();
+    trans_observations.clear();
+    Particle roi_particle = particles[i];
+    double particle_weights = 1;
+    // collect landmarks in sensor observating range.
+    for (int j = 0; j < map_landmarks.landmark_list.size(); j++) {
+      int land_id = map_landmarks.landmark_list[j].id_i;
+      float land_x = map_landmarks.landmark_list[j].x_f;
+      float land_y = map_landmarks.landmark_list[j].y_f;
+      double land_dist = pow((roi_particle.x - land_x), 2) + pow((roi_particle.y - land_y), 2);
+      land_dist = sqrt(land_dist);
+      if (land_dist <= sensor_range) {
+        LandmarkObs obs_landmark;
+        obs_landmark.id = land_id;
+        obs_landmark.x = land_x;
+        obs_landmark.y = land_y;
+        roi_landmarks.push_back(obs_landmark);
+      }
+    }
+    // transform observation
+    for (int j = 0; j < observations.size(); j++) {
+      LandmarkObs trans_obs_landmark;
+      double obs_x = observations[j].x;
+      double obs_y = observations[j].y;
+      trans_obs_landmark.x = roi_particle.x + (cos(roi_particle.theta) * obs_x) - (sin(roi_particle.theta) * obs_y);
+      trans_obs_landmark.y = roi_particle.y + (sin(roi_particle.theta) * obs_x) + (cos(roi_particle.theta) * obs_y);
+      trans_observations.push_back(trans_obs_landmark);
+    }
+    // association of observation and landmark
+    this->dataAssociation(roi_landmarks, trans_observations);
+
+    // update measurement probability
+    for (int j = 0; j < trans_observations.size(); j++) {
+      for (int k = 0; k < roi_landmarks.size(); k++) {
+        if (trans_observations[j].id == roi_landmarks[k].id) {
+          double exp_x = (trans_observations[j].x - roi_landmarks[k].x);
+          exp_x = exp_x * exp_x;
+          exp_x = exp_x / (2*std_landmark[0]*std_landmark[0]);
+          double exp_y = (trans_observations[j].y - roi_landmarks[k].y);
+          exp_y = exp_y * exp_y;
+          exp_y = exp_y / (2*std_landmark[1]*std_landmark[1]);
+          double roi_weights = exp(-1 * (exp_x + exp_y));
+          roi_weights = roi_weights / (2 * M_PI * std_landmark[0] * std_landmark[1]);
+             particle_weights *= roi_weights;
+        } // if 
+      } // for (k = )
+    } // for (j = )
+    weights_sum += particle_weights;
+    weights.push_back(particle_weights);
+  }
+
+  // normalize particle weights
+  for (int i = 0; i < weights.size(); i++) {
+    weights[i] = weights[i] / weights_sum;
+  }
 }
 
 void ParticleFilter::resample() {
 	// TODO: Resample particles with replacement with probability proportional to their weight. 
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+  default_random_engine rand_eng;
+  std::discrete_distribution<> d(weights.begin(), weights.end());
 
+  std::vector<Particle> new_particles;
+
+  for (int i = 0; i < num_particles; i++) {
+    int idx = d(rand_eng);
+    Particle new_particle;
+    new_particle.x = particles[idx].x;
+    new_particle.y = particles[idx].y;
+    new_particles.push_back(new_particle);
+  }
+  particles = new_particles;
+  
 }
 
 Particle ParticleFilter::SetAssociations(Particle& particle, const std::vector<int>& associations, 
@@ -126,26 +201,26 @@ string ParticleFilter::getAssociations(Particle best)
 {
 	vector<int> v = best.associations;
 	stringstream ss;
-    copy( v.begin(), v.end(), ostream_iterator<int>(ss, " "));
-    string s = ss.str();
-    s = s.substr(0, s.length()-1);  // get rid of the trailing space
-    return s;
+  copy( v.begin(), v.end(), ostream_iterator<int>(ss, " "));
+  string s = ss.str();
+  s = s.substr(0, s.length()-1);  // get rid of the trailing space
+  return s;
 }
 string ParticleFilter::getSenseX(Particle best)
 {
 	vector<double> v = best.sense_x;
 	stringstream ss;
-    copy( v.begin(), v.end(), ostream_iterator<float>(ss, " "));
-    string s = ss.str();
-    s = s.substr(0, s.length()-1);  // get rid of the trailing space
-    return s;
+  copy( v.begin(), v.end(), ostream_iterator<float>(ss, " "));
+  string s = ss.str();
+  s = s.substr(0, s.length()-1);  // get rid of the trailing space
+  return s;
 }
 string ParticleFilter::getSenseY(Particle best)
 {
 	vector<double> v = best.sense_y;
 	stringstream ss;
-    copy( v.begin(), v.end(), ostream_iterator<float>(ss, " "));
-    string s = ss.str();
-    s = s.substr(0, s.length()-1);  // get rid of the trailing space
-    return s;
+  copy( v.begin(), v.end(), ostream_iterator<float>(ss, " "));
+  string s = ss.str();
+  s = s.substr(0, s.length()-1);  // get rid of the trailing space
+  return s;
 }
